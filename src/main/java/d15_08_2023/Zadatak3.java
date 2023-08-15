f5bdbeb1623219ccf65bb64d0548106951c5ee04
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-08-15 23:56:57 +0200

    Homework exercises 15.08.2023

---
+++ src/main/java/d15_08_2023/Zadatak3.java
@@ -0,0 +1,15 @@
+package d15_08_2023;
+
+public class Zadatak3 {
+
+    public static void main(String[] args) {
+
+//      Napisati program koji racuna Z prema formuli: (X - Y) NA KVADRAT. X i Y su celi brojevi.
+
+        int x = 15;
+        int y = 5;
+        int z = (x - y) * (x - y);
+
+        System.out.println("Z je : " + z);
+    }
+}
