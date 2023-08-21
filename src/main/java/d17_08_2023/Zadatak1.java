04a94862ce508bb7d8a34c72677144cbff224246
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-08-21 14:13:28 +0200

    Adding task text within the code

--- src/main/java/d17_08_2023/Zadatak1.java
+++ src/main/java/d17_08_2023/Zadatak1.java
@@ -6,6 +6,18 @@ public class Zadatak1 {
 
     public static void main(String[] args) {
 
+//        Napisati program koji ima informacije koje ucitava informacije sa tastature:
+//        Ime
+//                Prezime
+//        godinu rodjenja
+//        I stampa informaicije u formatu:
+//[ime] [prezime] - [startost] god
+//
+//        Primer izvrsenja:
+//        Ako je ime = Milan, prezime = Jovanovic i godina rodjenja 1995 stapaju se informacije
+//        Milan Jovanovic - 26 god
+
+
         Scanner s = new Scanner(System.in);
 
         System.out.println("Unesite ime: ");
--- src/main/java/d17_08_2023/ZadatakZaVezbanje2.java
+++ src/main/java/d17_08_2023/ZadatakZaVezbanje2.java
@@ -6,6 +6,13 @@ public class ZadatakZaVezbanje2 {
 
     public static void main(String[] args) {
 
+//        Tea Tairovic je zatrazila od svojih najvernijih fanova ako mogu da joj izadju u susret i razviju softver za njene potrebe. Najveci fan Veljko se javio i iz razgovora je izvukao sta je potrebno da softver radi.
+//        Posto je izbacila 3 najveca hita, potrebno joj je da sracuna zaradu od pregleda na youtubu.
+//                Tea zaradjuje $1 na 1000 pregleda.
+//                Dok ne dodje do 50k pregleda, sve sto se zaradi sa youtube-a ostaje njoj.
+//        Ukoliko ima od 50k do 100k pregleda njen mendzer joj uzima fiksno $50, a sve preko 100k pregleda dele po pola.
+//                Program sa tastature ucitava ukupan broj pregleda jednog videa i ispisuje koliko ce Tea zaraditi od njega.
+
         Scanner s = new Scanner(System.in);
 
         System.out.println("Unesite ukupan broj pregleda: ");
