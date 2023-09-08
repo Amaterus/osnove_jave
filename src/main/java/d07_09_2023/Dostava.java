10a52f26bb1074c17d2270439d63ca4edb7022d8
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-09-08 14:11:51 +0200

    Add homework 07.09.2023

---
+++ src/main/java/d07_09_2023/Dostava.java
@@ -0,0 +1,9 @@
+package d07_09_2023;
+
+public class Dostava {
+    public Vozilo vozilo;
+    public Vozac vozac;
+    public Ruta ruta;
+    public String statusDostave;
+    public String datumDostave;
+}
